1573bdc44ccabf0cfc3f51e71051a5049634d4fc
maydindev <maydin.dev@outlook.com>
2024-09-20 14:38:46 +0300

    "Added useEffect hook to manage timer, introduced handleClickStart function, and modified button behavior"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import useSound from 'use-sound'
-import PlayArea from './components/PlayArea'
-import ScoreBoard from './components/ScoreBoard'
-import './styles.css'
+import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import PlayArea from "./components/PlayArea";
+import ScoreBoard from "./components/ScoreBoard";
+import "./styles.css";
 export default function App() {
-  const STARTING_TIME = 60
-  const STARTING_SCORE = 0
-  const [timerRunning, setTimerRunning] = useState(false)
-  const [timeLeft, setTimeLeft] = useState(STARTING_TIME)
-  const [score, setScore] = useState(STARTING_SCORE)
-  const [playSong] = useSound('../audio/song.mp3')
-  const [playClick] = useSound('../audio/click.mp3', { volume: 0.45 })
+  const STARTING_TIME = 60;
+  const STARTING_SCORE = 0;
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(STARTING_TIME);
+  const [score, setScore] = useState(STARTING_SCORE);
+  const [playSong] = useSound("../audio/song.mp3");
+  const [playClick] = useSound("../audio/click.mp3", { volume: 0.45 });
 
   /* Challenge 
 
@@ -29,11 +29,38 @@ export default function App() {
         4. Bu görevleri yerine getirmek için *sadece* bu yorumların altına kod yazmanız gerekir; bunların üzerinde veya farklı bir dosyada herhangi bir şey değiştirmeniz veya eklemeniz gerekmez 
 */
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (timeLeft > 0) {
+        setTimeLeft((prev) => prev - 1);
+      } else {
+        setTimerRunning(false);
+      }
+    }, 1000);
+    //console.log(timeLeft);
+    return () => clearInterval(intervalId);
+  }, [timeLeft]);
+
+  const handleClickStart = () => {
+    setTimerRunning(true);
+    setScore(0);
+    setTimeLeft((prev) => (prev !== STARTING_TIME ? STARTING_TIME : prev));
+    playSong();
+    playClick();
+  };
+
   return (
     <div>
       <ScoreBoard data={{ score, timeLeft }} />
       <PlayArea playProps={{ timeLeft, timerRunning, setScore }} />
-      <button className='play-button fade-in'>Başlat</button>
+      <button
+        className={
+          timerRunning ? "play-button fade-out" : "play-button fade-in"
+        }
+        onClick={() => handleClickStart()}
+      >
+        Başlat
+      </button>
     </div>
-  )
+  );
 }
